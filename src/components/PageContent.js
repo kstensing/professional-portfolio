5a78011c8786c72d10ce931bc9c97c4074aa9e27
kstensing <stensingk@gmail.com>
2022-05-09 23:15:46 -0500

    removed unused references

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import About from './pages/About';
 import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
